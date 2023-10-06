33ea92b720f079cb22303b8427c627e8242a68d5
Ninh LUU <quyninh3042003@gmail.com>
2023-10-06 18:21:34 +0700

    th2

---
+++ BTWeb1/Models/MenuItem.cs
@@ -0,0 +1,8 @@
+namespace BTWeb1.Models;
+
+public class MenuItem
+{
+    public int Id { get; set; }//Item id
+    public string Name { get; set; } //Item name
+    public string Link { get; set; } //Item label
+}
---
+++ BTWeb1/ViewComponents/RenderViewComponents.cs
@@ -0,0 +1,25 @@
+using BTWeb1.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace BTWeb1.ViewComponents;
+
+public class RenderViewComponents
+{
+    private List<MenuItem> MenuItems = new List<MenuItem>();
+
+    public RenderViewComponents()
+    {
+        MenuItems = new List<MenuItem>()
+        {
+            new MenuItem() {Id = 1, Name = "Branches", Link = "Branches/List"},
+            new MenuItem() {Id = 2, Name = "Students", Link = "Students/List"},
+            new MenuItem() {Id = 3, Name = "Subjects", Link = "Subjects/List"},
+            new MenuItem() {Id = 4, Name = "Courses", Link = "Courses/List"}
+        };
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+      
+        return View("RenderLeftMenu", MenuItems);
+    }
+}
